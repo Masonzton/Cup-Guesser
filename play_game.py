a849f1a1ebba5e02418b8e837dd505217ee61c96
Mason Notz <masonzton1@gmail.com>
2024-08-05 20:52:56 -0700

    Reformat Code

--- play_game.py
+++ play_game.py
@@ -1,32 +1,38 @@
 import random
 import itertools
-from typing import List, Tuple
+from typing import List
 
 NUMBER_OF_DIGITS = 4
-ALL_POSSIBLE_CODES = list(itertools.permutations(range(1, NUMBER_OF_DIGITS+1)))
+ALL_POSSIBLE_CODES = list(itertools.permutations(range(1, NUMBER_OF_DIGITS + 1)))
+
 
 def get_random_code(N):
     numbers = list(range(1, N + 1))
     random.shuffle(numbers)
     return numbers
 
+
 def get_number_correct(array, guess):
     correct = 0
     for index in range(len(array)):
         if int(array[index]) == int(guess[index]):
             correct += 1
-    
+
     return correct
+
 
 def guess_to_array(guess) -> List[int]:
     return [int(i) for i in guess]
+
 
 def print_guess_distribution(remaining_codes: List[List[int]]) -> List[List[int]]:
     """For each guess, there will be a certain distribution of 0,1,2, or 4 correct
     cups. A guess is good when it has a good spread on this distribution. This is because
     you want there to be the least number of remaining possible guesses. So the ideal guess
     is the one with the smallest maximum"""
-    print("Distribution of correct cups: number of guesses that achieve this distribution")
+    print(
+        "Distribution of correct cups: number of guesses that achieve this distribution"
+    )
     distributions = {}
     for guess in ALL_POSSIBLE_CODES:
         # number of codes in each category of number correct
@@ -55,27 +61,26 @@ def print_guess_distribution(remaining_codes: List[List[int]]) -> List[List[int]
                 best_guess = codes[0]
                 best_distribution = distribution
 
-            
         print(f"{distribution}: {len(codes)} example: {codes[0]}")
-    
+
     print(f"greedy guess: {best_guess}")
-    
+
 
 def is_guess_valid(guess: str) -> bool:
     is_valid = True
-    is_valid &= (len(guess) == NUMBER_OF_DIGITS)
+    is_valid &= len(guess) == NUMBER_OF_DIGITS
     for number in guess:
         is_valid &= number.isnumeric()
         if number.isnumeric():
             is_valid &= int(number) <= NUMBER_OF_DIGITS
             is_valid &= int(number) > 0
-    
+
     return is_valid
 
 
 def play_game(debug=False):
     if debug:
-        remaining_codes = list(itertools.permutations(range(1, NUMBER_OF_DIGITS+1)))
+        remaining_codes = list(itertools.permutations(range(1, NUMBER_OF_DIGITS + 1)))
     actual_numbers = get_random_code(NUMBER_OF_DIGITS)
 
     # print(f"Secret Numbers are: {actual_numbers}")
@@ -85,8 +90,10 @@ def play_game(debug=False):
             print_guess_distribution(remaining_codes)
 
         # ask for input
-        example_string = "".join([str(x) for x in range(1,NUMBER_OF_DIGITS+1)])
-        guess = input(f"\n\nInput Guess, numbers 1 to {NUMBER_OF_DIGITS}. i.e. {example_string}:  ")
+        example_string = "".join([str(x) for x in range(1, NUMBER_OF_DIGITS + 1)])
+        guess = input(
+            f"\n\nInput Guess, numbers 1 to {NUMBER_OF_DIGITS}. i.e. {example_string}:  "
+        )
         if guess == "q":
             break
 
@@ -102,9 +109,13 @@ def play_game(debug=False):
         print(f"{number_correct} Are correct")
 
         if debug:
-            remaining_codes = [code for code in remaining_codes if get_number_correct(guess, code) == number_correct]
+            remaining_codes = [
+                code
+                for code in remaining_codes
+                if get_number_correct(guess, code) == number_correct
+            ]
             print(f"Total Remaining Codes: {len(remaining_codes)}")
-            print(f"Reaming codes are:")
+            print("Reaming codes are:")
             for code in remaining_codes:
                 print(code)
 
--- test_greedy_strat.py
+++ test_greedy_strat.py
@@ -1,20 +1,24 @@
 """Test the greedy strategy"""
+
 from typing import List, Dict, Tuple
 import itertools
 
 NUMBER_OF_DIGITS = 4
-ALL_POSSIBLE_CODES = list(itertools.permutations(range(1, NUMBER_OF_DIGITS+1)))
+ALL_POSSIBLE_CODES = list(itertools.permutations(range(1, NUMBER_OF_DIGITS + 1)))
+
 
 def get_number_correct(code, guess):
     correct = 0
     for index in range(len(code)):
         if code[index] == guess[index]:
             correct += 1
-    
+
     return correct
 
 
-def get_distributions(remaining_codes: List[List[int]]) -> Dict[Tuple[int], List[List[int]]]:
+def get_distributions(
+    remaining_codes: List[List[int]],
+) -> Dict[Tuple[int], List[List[int]]]:
     distributions = {}
     for guess in ALL_POSSIBLE_CODES:
         # number of codes in each category of number correct
@@ -31,6 +35,7 @@ def get_distributions(remaining_codes: List[List[int]]) -> Dict[Tuple[int], List
 
     return distributions
 
+
 def get_greedy_guess(distributions: Dict[Tuple, List[List[int]]]) -> List[int]:
     smallest_maximum = None
     best_distribution = None
@@ -46,8 +51,9 @@ def get_greedy_guess(distributions: Dict[Tuple, List[List[int]]]) -> List[int]:
                 smallest_maximum = maximum
                 best_guess = codes[0]
                 best_distribution = distribution
-    
+
     return best_guess
+
 
 def get_filter_codes(remaining_codes, guess, number_correct):
     filtered_codes = []
@@ -55,6 +61,7 @@ def get_filter_codes(remaining_codes, guess, number_correct):
         if get_number_correct(code, guess) == number_correct:
             filtered_codes.append(code)
     return filtered_codes
+
 
 def main():
     # simulate greedy guessing for every possible actual code
@@ -71,20 +78,20 @@ def main():
             #     print(f"{distribution}: {len(codes)} example: {codes[0]}")
 
             number_correct = get_number_correct(actual_code, guess)
-            number_of_guesses+=1
+            number_of_guesses += 1
 
             # print(f"guess: {guess}, number correct: {number_correct}")
 
             if number_correct == NUMBER_OF_DIGITS:
                 break
-            
+
             remaining_codes = get_filter_codes(remaining_codes, guess, number_correct)
-        
+
         print(f"code: {actual_code}, number_of_guesses: {number_of_guesses}")
-        
+
         if worst_case_guesses is None or number_of_guesses > worst_case_guesses:
             worst_case_guesses = number_of_guesses
-    
+
     print(f"worst case is {worst_case_guesses} guesses with greedy strategy")
 
 
