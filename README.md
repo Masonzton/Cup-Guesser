4ad3438ce701535615ef0b5025dcba585af8c2b3
Mason Notz <masonzton1@gmail.com>
2024-08-06 06:47:52 -0700

    Add some histograms for 4,5,6

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 Based on the tik tok trend where you try and guess the order of the cups.
 https://www.tiktok.com/@klemfamily/video/7329231012624403758
 
-play_game is for playing the game. Also tried out one strategy called the greedy strategy. This is choosing the guess that will result in the best worst case outcome at each step. After each guess, there will be a certain number of remaining permutations that are valid sequences. So working backwards from these candidate permutations, for each guess possible each of the candidate permutation will either have 0,1,2, or 4 correct digits. So the goal is to pick a guess such that each category is roughly equal. 
-This strategy can always get the right sequence in 5 guesses. (But you will know the sequency after the 4th guess).
+play_game is for playing the game. Also tried out one strategy called the greedy strategy. This is choosing the guess that will result in the best worst case outcome at each step. After each guess, there will be a certain number of remaining permutations that could be the actual code. So working backwards from these candidate permutations, for each guess possible each of the candidate permutation will either have 0,1,2, or 4 correct digits. So the goal is to pick a guess such that each category is roughly equal. Specifically, the guess that will results in the smallest largest bin.
 
-There might be a better strategy. But haven't been able to find it yet.
-Interested to see how things are different for 6 cups instead of 4 cups. Also if you start adding extra rules.
-Like you must move at least 3 cups for each guess. Another question is what the average or expected number of guesses are for a strategy and what the minimum number is.
-Also maybe there is a strategy that is easier to remember and doesn't involve keeping track of exactly which combinations are left.
+Testing the greedy strategy for 4,5,6 cups. It seems that the maximum number of guesses to know the sequence is equal to the number of cups. you can see the pictures of the histograms to see the distribution of results as well. Actually takes N+1 guesses since you need to do the last guess even if you already know the correct solution.
+
+Things to look into: 
+
+- There might be a better strategy and maybe one that is simpler / easier for a human to do. But haven't been able to find it yet.
+- Does the pattern of requiring N guesses for N cups continue?
+- Interested to see how things are different for adding extra rules. For example: you must move at least 3 cups for each guess.
--- test_greedy_strat.py
+++ test_greedy_strat.py
@@ -2,6 +2,7 @@
 
 from typing import List, Dict, Tuple
 import itertools
+import matplotlib.pyplot as plt
 
 NUMBER_OF_DIGITS = 4
 ALL_POSSIBLE_CODES = list(itertools.permutations(range(1, NUMBER_OF_DIGITS + 1)))
@@ -22,7 +23,7 @@ def get_distributions(
     distributions = {}
     for guess in ALL_POSSIBLE_CODES:
         # number of codes in each category of number correct
-        distribution = [0, 0, 0, 0, 0]
+        distribution = [0 for _ in range(NUMBER_OF_DIGITS + 1)]
         # see how many would be eliminate at this step given the remaining codes
         for code in remaining_codes:
             number_correct = get_number_correct(code, guess)
@@ -36,6 +37,15 @@ def get_distributions(
     return distributions
 
 
+def count_nonzero(array: List[int]) -> int:
+    nonzero = 0
+    for number in array:
+        if number != 0:
+            nonzero += 1
+
+    return nonzero
+
+
 def get_greedy_guess(distributions: Dict[Tuple, List[List[int]]]) -> List[int]:
     smallest_maximum = None
     best_distribution = None
@@ -45,12 +55,23 @@ def get_greedy_guess(distributions: Dict[Tuple, List[List[int]]]) -> List[int]:
             smallest_maximum = maximum
             best_guess = codes[0]
             best_distribution = distribution
+            best_nonzero = count_nonzero(distribution)
         elif maximum == smallest_maximum:
-            # it is better to have a guess in the all correct category
-            if distribution[-1] >= best_distribution[-1]:
+            current_non_zero = count_nonzero(distribution)
+            if current_non_zero > best_nonzero:
                 smallest_maximum = maximum
                 best_guess = codes[0]
                 best_distribution = distribution
+                best_nonzero = current_non_zero
+            elif (
+                current_non_zero == best_nonzero
+                and distribution[-1] >= best_distribution[-1]
+            ):
+                # it is better to have a guess in the all correct category
+                smallest_maximum = maximum
+                best_guess = codes[0]
+                best_distribution = distribution
+                best_nonzero = current_non_zero
 
     return best_guess
 
@@ -67,6 +88,8 @@ def main():
     # simulate greedy guessing for every possible actual code
     worst_case_guesses = None
     total_codes = len(ALL_POSSIBLE_CODES)
+    history = []
+    print(f"testing {total_codes} codes")
     for actual_code in ALL_POSSIBLE_CODES:
         number_of_guesses = 0
         remaining_codes = ALL_POSSIBLE_CODES
@@ -87,12 +110,31 @@ def main():
 
             remaining_codes = get_filter_codes(remaining_codes, guess, number_correct)
 
-        print(f"code: {actual_code}, number_of_guesses: {number_of_guesses}")
+        # print(f"code: {actual_code}, number_of_guesses: {number_of_guesses}")
+        history.append(number_of_guesses)
+
+        if len(history) % 10 == 0:
+            print(f"have tested: {len(history)}")
 
         if worst_case_guesses is None or number_of_guesses > worst_case_guesses:
             worst_case_guesses = number_of_guesses
 
     print(f"worst case is {worst_case_guesses} guesses with greedy strategy")
+    min_data = min(history)
+    max_data = max(history)
+    # +2 to include the last value as a bin edge
+    bin_edges = [x - 0.5 for x in range(min_data, max_data + 2)]
+
+    # Create histogram
+    plt.hist(history, bins=bin_edges, edgecolor="black")
+
+    # Add title and labels
+    plt.title("Histogram")
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+
+    # Save the plot to a file
+    plt.savefig(f"histogram_{NUMBER_OF_DIGITS}.png")
 
 
 if __name__ == "__main__":
